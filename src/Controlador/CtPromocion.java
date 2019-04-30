52d8f8cda40b39ca4a0dbb3f4088588fa590c79c
mateo <mateo@LAPTOP-GHAV4213.mshome.net>
2019-04-29 22:47:32 -0500

    termine el ctPromocion

--- src/Controlador/CtPromocion.java
+++ src/Controlador/CtPromocion.java
@@ -85,56 +85,73 @@ public class CtPromocion {
 
     public ClsPromocion buscarPromocion(ArrayList<ClsPromocion> listaPromocion, String idPromocion) {
         ClsPromocion promocion = null;
-        int encontrado = 0;
-        for (int i = 0; i < listaPromocion.size(); i++) {
-            if (idPromocion.equalsIgnoreCase(listaPromocion.get(i).getIdPromocion())) {
-                promocion = listaPromocion.get(i);
-                encontrado++;
-                break;
+        try {
+
+            int encontrado = 0;
+            for (int i = 0; i < listaPromocion.size(); i++) {
+                if (idPromocion.equalsIgnoreCase(listaPromocion.get(i).getIdPromocion())) {
+                    promocion = listaPromocion.get(i);
+                    encontrado++;
+                    break;
+                }
             }
-        }
-        if (encontrado == 0) {
-            JOptionPane.showMessageDialog(null, "No se encontro ningúna promocion ese id");
+            if (encontrado == 0) {
+                JOptionPane.showMessageDialog(null, "No se encontro ningúna promocion ese id");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
         }
         return promocion;
     }
 
     public ArrayList<ClsPromocion> eliminarPromocion(ArrayList<ClsPromocion> listaPromocion, String idPromocion) {
-        for (int i = 0; i < listaPromocion.size(); i++) {
-            if (idPromocion.equalsIgnoreCase(listaPromocion.get(i).getIdPromocion())) {
-                listaPromocion.remove(i);
-                JOptionPane.showMessageDialog(null, "Promocion eliminada");
-            } else {
-                JOptionPane.showMessageDialog(null, "No se encontro ningúna promocion ese id");
+        try {
+            for (int i = 0; i < listaPromocion.size(); i++) {
+                if (idPromocion.equalsIgnoreCase(listaPromocion.get(i).getIdPromocion())) {
+                    listaPromocion.remove(i);
+                    JOptionPane.showMessageDialog(null, "Promocion eliminada");
+                } else {
+                    JOptionPane.showMessageDialog(null, "No se encontro ningúna promocion ese id");
+                }
             }
+        } catch (Exception e) {
+            System.out.println(e.toString());
         }
         return listaPromocion;
     }
 
     public ArrayList<ClsPromocion> modificarPromocion(ArrayList<ClsPromocion> listaPromocion, String nombrePromocion, String fecha, String idPromocion, double equipaje, double millas, double valorPromocion) {
-        for (int i = 0; i < listaPromocion.size(); i++) {
-            if (idPromocion.equalsIgnoreCase(listaPromocion.get(i).getIdPromocion())) {
-                listaPromocion.get(i).setNombrePromocion(nombrePromocion);
-                listaPromocion.get(i).setFecha(fecha);
-                listaPromocion.get(i).setEquipaje(equipaje);
-                listaPromocion.get(i).setMillas(millas);
-                JOptionPane.showMessageDialog(null, "Promocion modificada");
-            } else {
-                JOptionPane.showMessageDialog(null, "no se encontro ninguna promocion con ese id");
+        try {
+            for (int i = 0; i < listaPromocion.size(); i++) {
+                if (idPromocion.equalsIgnoreCase(listaPromocion.get(i).getIdPromocion())) {
+                    listaPromocion.get(i).setNombrePromocion(nombrePromocion);
+                    listaPromocion.get(i).setFecha(fecha);
+                    listaPromocion.get(i).setEquipaje(equipaje);
+                    listaPromocion.get(i).setMillas(millas);
+                    JOptionPane.showMessageDialog(null, "Promocion modificada");
+                } else {
+                    JOptionPane.showMessageDialog(null, "no se encontro ninguna promocion con ese id");
+                }
             }
+        } catch (Exception e) {
+            System.out.println(e.toString());
         }
         return listaPromocion;
 
     }
-    public DefaultTableModel listarElementos(ArrayList<ClsPromocion>listaPromocion) {
+
+    public DefaultTableModel listarElementos(ArrayList<ClsPromocion> listaPromocion) {
         DefaultTableModel modelo;
         String nombreColumnas[] = {"Fecha", "idPromocion", "Nombre promocion", "Equipaje", "Millas", "Valor Promocion"};
         modelo = new DefaultTableModel(new Object[][]{}, nombreColumnas);
-
-        for (int i = 0; i < listaPromocion.size(); i++) {
-            modelo.addRow(new Object[]{listaPromocion.get(i).getFecha(), listaPromocion.get(i).getIdPromocion(), listaPromocion.get(i).getNombrePromocion(), listaPromocion.get(i).getEquipaje(), listaPromocion.get(i).getMillas(), listaPromocion.get(i).getValorPromocion()});
+        try {
+            for (int i = 0; i < listaPromocion.size(); i++) {
+                modelo.addRow(new Object[]{listaPromocion.get(i).getFecha(), listaPromocion.get(i).getIdPromocion(), listaPromocion.get(i).getNombrePromocion(), listaPromocion.get(i).getEquipaje(), listaPromocion.get(i).getMillas(), listaPromocion.get(i).getValorPromocion()});
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
         }
-
         return modelo;
     }
 }
