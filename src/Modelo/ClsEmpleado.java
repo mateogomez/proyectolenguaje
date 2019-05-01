b2b89e41ae084dff16d2f53ac53e1de802a2d874
mateo <mateo@LAPTOP-GHAV4213.mshome.net>
2019-04-30 22:41:03 -0500

    cree las clases ClsEmpleado y ClsUsuario

---
+++ src/Modelo/ClsEmpleado.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author mateo
+ */
+public class ClsEmpleado extends ClsUsuario {
+    String cargo;
+
+    public ClsEmpleado(String cargo, String nombre, String apellido, String genero, String cedula, String correo, String telefono, String password) {
+        super(nombre, apellido, genero, cedula, correo, telefono, password);
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    
+    
+}
---
+++ src/Modelo/ClsUsuario.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author mateo
+ */
+public class ClsUsuario {
+    String nombre;
+    String apellido;
+    String genero;
+    String cedula;
+    String correo;
+    String telefono;
+    String password;
+
+    public ClsUsuario(String nombre, String apellido, String genero, String cedula, String correo, String telefono, String password) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.cedula = cedula;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
